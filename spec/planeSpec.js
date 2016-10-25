a56059c9b56fc386cac8951f53a3813a81cd6797
Felix Harrison <fmlharrison@gmail.com>
2016-10-25 14:20:56 +0100

    Added the plane.js and tested for flying

---
+++ spec/planeSpec.js
@@ -0,0 +1,25 @@
+describe("Plane", function (){
+  var plane;
+
+  beforeEach(function (){
+    plane = new Plane();
+  });
+
+  describe("the plane is created flying", function () {
+
+    it("should be flying when created", function () {
+      expect(plane.flying).toBeTruthy();
+    });
+
+    it("should not be flying when landed", function () {
+      plane.landed();
+      expect(plane.flying).toBeFalsy();
+    });
+
+    it("should be flying after take off", function () {
+      plane.landed();
+      plane.takenOff();
+      expect(plane.flying).toBeTruthy();
+    })
+  });
+});
--- src/plane.js
+++ src/plane.js
@@ -1,3 +1,11 @@
 function Plane() {
-  
+  this.flying = true
+};
+
+Plane.prototype.landed = function () {
+  this.flying = false;
+};
+
+Plane.prototype.takenOff = function () {
+  this.flying = true;
 };
